6b78d8402695198b4e483b5aeecce83a1dbe34ec
Shaikh Mohammed Sahil <correct.smsahil00971@gmail.com>
2025-06-02 23:56:08 +0530

    Update About page with latest changes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -225,7 +225,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1VA-5TzS7cnUpzUVOo8jYtTcO5kT9JR4y/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1_KCuIexYd_Nx6sjGPz8gwPFmBwDAiq9l/view?usp=sharing" className="w-full lg:w-auto">
                 <button 
                   data-aos="fade-up" 
                   data-aos-duration="800"
